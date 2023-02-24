b9073ae8973e35390dcba61ac527efd753198bde
jkryszto00 <jakubkryszto@gmail.com>
2023-02-25 00:54:14 +0100

    teams and invites routing

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('team/create', [\App\Http\Controllers\Team\TeamController::class, 'create'])->name('teams.create');
+    Route::post('team', [\App\Http\Controllers\Team\TeamController::class, 'store'])->name('teams.store');
+    Route::get('team', [\App\Http\Controllers\Team\TeamController::class, 'show'])->name('teams.show');
+
+    Route::post('team/{user}/detach', [\App\Http\Controllers\Team\MemberController::class, 'detachUser'])->name('teams.members.detach');
+
+    Route::post('team/invite-member', [\App\Http\Controllers\Team\InviteController::class, 'store'])->name('teams.member.invite');
+    Route::get('team/invite/{token}/accept', [\App\Http\Controllers\Team\InviteController::class, 'accept'])->name('teams.member.invite.accept');
+    Route::delete('team/invite/{invite}', [\App\Http\Controllers\Team\InviteController::class, 'destroy'])->name('teams.member.invite.destroy');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
